83344a708d4b16e14c0580598f360e272d214b78
Vangelis Galanis <vaggalanis@gmail.com>
2021-10-19 20:40:25 +0300

    fix: make project work properly for multiple server environments

--- src/app/readings.service.ts
+++ src/app/readings.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Reading } from 'src/typings';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,6 @@ export class ReadingsService {
   constructor(private http: HttpClient) {}
 
   getReadings(): Observable<Reading[]> {
-    return this.http.get<Reading[]>(document.location.origin + '/assets/data/sensor_readings_correct.json');
+    return this.http.get<Reading[]>(environment.serverUrl + '/assets/data/sensor_readings_correct.json');
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  serverUrl: 'localhost:4200',
 };
 
 /*
